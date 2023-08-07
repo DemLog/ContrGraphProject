cbad371f1f9f95a902dd55570d7de5018a17d4df
DemLoveSky <dem.loginov@gmail.com>
2023-08-07 17:09:34 +0500

    Refactoring starting project

--- react-project/src/components/ContributionGraph/index.tsx
+++ react-project/src/components/ContributionGraph/index.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import {ContributionGraphProps} from "@components/ContributionGraph/props";
 
+import './style.scss';
+
 export const ContributionGraph: React.FC<ContributionGraphProps> = (props: ContributionGraphProps) => {
     return (
-        <div></div>
+        <div className='contribution-graph'>
+            <table>
+
+            </table>
+            <div className='contribution-graph-legend'>
+
+            </div>
+        </div>
     );
 };
